b85ad778346ae0c3a0f88bac5e8265bb258cc2f0
jjmadrigap <53029570+jjmadrigap@users.noreply.github.com>
2023-03-25 08:33:41 -0500

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.8
+
+# working directory
+WORKDIR /user/src/app
+
+# copy all files to the container
+COPY . .
+
+# Install pip requirements
+RUN python -m pip install --no-cache-dir -r requirements.txt
+
+# port number to expose
+EXPOSE 8080
+
+# run the web service on the container
+CMD ["python", "crearTransacciones.py"]
---
+++ crearTransacciones.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify, request, render_template
+import os
+import requests
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+url = 'http://facb.sflorezs1.tk/transacciones'
+@app.route('/templates')
+def template():
+    return render_template('template.html')
+
+@app.route('/', methods=['POST', 'GET'])
+def generarMovimiento():
+    if request.method == 'POST':
+        umaiz = int(request.form['maiz'])
+        ucocoa = int(request.form['cocoa'])
+        uarroz = int(request.form['arroz'])
+        tipo = request.form['tipoMovimiento']
+        valor = umaiz*5000 + ucocoa*8000 + uarroz*4000
+        detalles = "Unidades de maiz a 5000: "+str(umaiz)+" \nUnidades de cocoa a 8000: "+str(ucocoa)+" \n Unidades de arroz a 4000: "+str(uarroz)
+        cliente = request.form['cliente']
+        
+        payload = {'type': tipo, 'value': valor, 'detalles': detalles, 'cliente': cliente}
+        print(payload)
+        response = requests.post(url, json=payload)
+        
+        if response.status_code == 200:
+            return "Transacción realizada exitosamente"
+        else: 
+            return "Error: " + str(response.status_code)
+    else:
+        return render_template('crearTransaccion.html')
+    
+app.run(debug=True, host='0.0.0.0',port=8080)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0',port=8080)
